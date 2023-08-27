7e188cb28b9b9a8a505f336ecd2b04769d2a17ff
mahdi rezainiya <mahdi.rezainiya@gmail.com>
2023-08-27 19:41:02 +0330

    new edited

--- part-1/sandbox.ts
+++ part-1/sandbox.ts
@@ -1,7 +1,87 @@
-const charc = "mahdi" ;
-console.log(charc);
-const inputs = document.querySelectorAll('input');
-console.log(inputs);
-inputs.forEach(input => {
-    console.log(input);
-})
+// const charc = "mahdi" ;
+// console.log(charc);
+// const inputs = document.querySelectorAll('input');
+// console.log(inputs);
+// inputs.forEach(input => {
+//     console.log(input);
+// })
+// let fName = "mahdi" ;
+// fName = 44;
+// let age = 25 ;
+// age = "ali" ;
+// let isMale = true ;
+// isMale = "mahdi" ;
+
+// const circ = (diameter : number) => {
+//     return diameter * Math.PI
+// }
+// console.log(circ(12));
+
+// let names = ["mahdi" , "ali" , "hamed" , "omid"];
+// names = "mahdi" ;
+// names[1] = 24 ;
+// names[1] = "mohammad" ;
+// names.push("string1");
+// console.log(names);
+
+// let numbers = [10 , 20 , 30 , 40 , 50 , 60];
+// numbers[1] = 80 ;
+// numbers.push(50);
+// console.log(numbers);
+
+// let mixed = [10 , 20 , 30 , "mahdi" , "ali"];
+// mixed[0] = "subhan" ;
+// mixed[1] = 10 ;
+// mixed[2] = true ;
+// console.log(mixed);
+
+// let person = {name : "mahdi" , age : 25 , isMale : true };
+// person = [];
+// person.name = 10 ;
+// console.log(person);
+
+// explicit type
+
+// let fName = "mahdi" ;
+// let fName : string | number | boolean ;
+// fName = "mahdi";
+// fName = 12 ;
+// fName = true ;
+// console.log(fName);
+
+// let mixed: (number | string )[] = [];
+// mixed[0] = 14 ;
+// mixed.push("mahdi");
+// console.log(mixed);
+
+// let person : object ;
+// personone = [] ;
+// console.log(person);
+
+// let personTwo : {name : string , age : number , isMale : true};
+// personTwo = {name : "mahdi" , age : 25 , isMale : true};
+// personTwo.phone = 40 ;
+// console.log(personTwo);
+
+// let age : any ;
+// age = 12 ;
+// age = "mahdi" ;
+// age = true ;
+// console.log(age);
+
+// let mixed : any[] = [] ;
+// mixed.push(12);
+// console.log(mixed);
+
+// let person : {name : any , age : any} ;
+// person = {name : 12 , age : "mahdi"};
+// console.log(person);
+
+
+
+
+
+
+
+
+
