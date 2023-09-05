e5bd307e93b7d24f43e018dc73b7ae4b3d08f5ee
mahdi rezainiya <mahdi.rezainiya@gmail.com>
2023-09-05 16:15:45 +0330

    New Changes

--- part-1/src/app.ts
+++ part-1/src/app.ts
@@ -180,20 +180,62 @@
 
 // console.log(form.children);
 
-
-const form = document.querySelector('.new-item-form') as HTMLFormElement;
+// const form = document.querySelector('.new-item-form') as HTMLFormElement;
 //inputs
-const type = document.querySelector('#type') as HTMLSelectElement;
-const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
-const details = document.querySelector('#details') as HTMLInputElement;
-const amount = document.querySelector('#amount') as HTMLInputElement;
+// const type = document.querySelector('#type') as HTMLSelectElement;
+// const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
+// const details = document.querySelector('#details') as HTMLInputElement;
+// const amount = document.querySelector('#amount') as HTMLInputElement;
 
-form.addEventListener('submit', (e: Event) => {
-    e.preventDefault();
-    console.log(
-        type.value,
-        tofrom.value,
-        details.value,
-        amount.valueAsNumber
-    );
-})
+// form.addEventListener('submit', (e: Event) => {
+//     e.preventDefault();
+//     console.log(
+//         type.value,
+//         tofrom.value,
+//         details.value,
+//         amount.valueAsNumber
+//     );
+// })
+
+// class Invoice {
+//     public client : string ;
+//     private details : string ;
+//     readonly amout : number
+//     constructor(c : string , d : string , a : number){
+//         this.client = c ;
+//         this.details = d ;
+//         this.amout = a 
+//     }
+    // format(){
+    //     return `${this.client} owes ${this.amout} for ${this.details}`
+    // }
+// }
+// const invOne = new Invoice("mahdi" , "web design" , 240);
+// const invTwo = new Invoice("ali" , "ux & ui" , 40);
+
+// console.log(invOne.format());
+// console.log(invTwo.format());
+
+// let invoices : Invoice[] = [] ;
+// invoices.push(invOne)
+// invoices.push(invTwo)
+// console.log(invoices);
+
+// for(let inv of invoices){
+    // console.log(inv.client);
+    // console.log(inv.details);
+    // console.log(inv.amout);
+// }
+
+class Invoice{
+    constructor(public client : string , private details : string , readonly amount : number){
+        
+    }
+    format(){
+        return `${this.client} owes ${this.amount} for ${this.details}`
+    }
+}
+
+
+
+
--- public/app.js
+++ public/app.js
@@ -140,54 +140,13 @@
 // console.log(anchor.href);
 // const form = document.querySelector('form')!;
 // console.log(form.children);
-// const form = document.querySelector('.new-item-form') as HTMLFormElement;
+const form = document.querySelector('.new-item-form');
 //inputs
-// const type = document.querySelector('#type') as HTMLSelectElement;
-// const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
-// const details = document.querySelector('#details') as HTMLInputElement;
-// const amount = document.querySelector('#amount') as HTMLInputElement;
-// form.addEventListener('submit', (e: Event) => {
-//     e.preventDefault();
-//     console.log(
-//         type.value,
-//         tofrom.value,
-//         details.value,
-//         amount.valueAsNumber
-//     );
-// })
-// class Invoice {
-//     public client : string ;
-//     private details : string ;
-//     readonly amout : number
-//     constructor(c : string , d : string , a : number){
-//         this.client = c ;
-//         this.details = d ;
-//         this.amout = a 
-//     }
-// format(){
-//     return `${this.client} owes ${this.amout} for ${this.details}`
-// }
-// }
-// const invOne = new Invoice("mahdi" , "web design" , 240);
-// const invTwo = new Invoice("ali" , "ux & ui" , 40);
-// console.log(invOne.format());
-// console.log(invTwo.format());
-// let invoices : Invoice[] = [] ;
-// invoices.push(invOne)
-// invoices.push(invTwo)
-// console.log(invoices);
-// for(let inv of invoices){
-// console.log(inv.client);
-// console.log(inv.details);
-// console.log(inv.amout);
-// }
-class Invoice {
-    constructor(client, details, amount) {
-        this.client = client;
-        this.details = details;
-        this.amount = amount;
-    }
-    format() {
-        return `${this.client} owes ${this.amount} for ${this.details}`;
-    }
-}
+const type = document.querySelector('#type');
+const tofrom = document.querySelector('#tofrom');
+const details = document.querySelector('#details');
+const amount = document.querySelector('#amount');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+});
